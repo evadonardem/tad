0b0ed831b2742092d9e07ea2d5dc7d8138355d77
Dave Medrano <evadonardem@gmail.com>
2019-10-23 10:50:20 +0800

    General updates

--- database/seeds/GeneralDatabaseSeeder.php
+++ database/seeds/GeneralDatabaseSeeder.php
@@ -14,23 +14,14 @@ class GeneralDatabaseSeeder extends Seeder
     public function run()
     {
         $users = User::all();
-        /*$users = $users->filter(function ($user) {
+        $users = $users->filter(function ($user) {
           return $user->types->count() === 0;
-        });*/
+        });
 
         $users->each(function ($user) {
-          /*$type = $user->types()->create([
+          $type = $user->types()->create([
             'type' => 'FACULTY'
-          ]);*/
-          $type = $user->types()->first();
-          /*$firstLog = $user->attendanceLogs()
-            ->orderBy('biometric_timestamp', 'asc')
-            ->first();
-          if($firstLog) {
-            $type->created_at = $type->updated_at = Carbon::parse($firstLog->biometric_timestamp)
-              ->format('Y-m-d H:i:s');
-            $type->save();
-          }*/
+          ]);
           $type->created_at = $type->updated_at = '2019-01-01 00:00:00';
           $type->save();
         });
